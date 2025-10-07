1763928d8d72a241b4cce57b7a463a44719a99f3
BenjaminCollao-A <bender1999@live.com>
2025-10-06 21:07:33 -0400

    Create rooms.js

---
+++ src/data/rooms.js
@@ -0,0 +1,21 @@
+// src/data/rooms.js
+export const rooms = [
+  {
+    id: 1,
+    name: 'Suite Deluxe',
+    price: 150,
+    image: 'https://example.com/suite-deluxe.jpg',
+  },
+  {
+    id: 2,
+    name: 'Habitación Estándar',
+    price: 100,
+    image: 'https://example.com/habitacion-estandar.jpg',
+  },
+  {
+    id: 3,
+    name: 'Habitación Familiar',
+    price: 180,
+    image: 'https://example.com/habitacion-familiar.jpg',
+  },
+];
