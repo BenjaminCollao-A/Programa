f502c89031978b334c58ef8ee2fe7b5666e79847
BenjaminCollao-A <bender1999@live.com>
2025-10-06 21:48:40 -0400

    Create metrics.js

---
+++ src/data/metrics.js
@@ -0,0 +1,7 @@
+// src/data/metrics.js
+export const metrics = {
+  occupancyCurrent: 75,      // %
+  upcomingReservations: 12,
+  revenue: 15000,            // currency
+  monthlyOccupancy: [55, 70, 60, 80, 72, 75] // Ene..Jun
+};
